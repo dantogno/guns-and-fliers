534ebc20849027dd40e4dc3ab452c0efaa72b446
David Antognoli <david.antognoli@gmail.com>
2019-03-18 19:05:35 -0500

    working on player movement and test dialog scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3;
+
+    private new Rigidbody2D rigidbody2D;
+    private Vector2 movementInput = Vector2.zero;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        movementInput.x = Input.GetAxis("Horizontal");
+        movementInput.y = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = movementInput * speed;
+    }
+}
